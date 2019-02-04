9225c86d9b15406eabae179e7da8a3d669f1146f
Liz Steak <lizmaepark@gmail.com>
2019-02-04 10:09:41 -0700

    changed mySQL pw

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Indigo37*'
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'addresses'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
